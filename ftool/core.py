2ac0e76d69efe6a5029bc95a19f3e563c9b3aa68
r-tamura <rtamura30@gmail.com>
2018-12-23 18:45:49 +0900

    再実装

--- ftool/core.py
+++ ftool/core.py
@@ -1,73 +1,241 @@
 import builtins
-import collections
 import functools
 
 __all__ = (
-  "curry", "compose",
-  "always", "identity",
-  "map", "flatten", "flatmap",
+  "curry", "compose", "pipe",
+  "T", "F", "identity", "always", "alternation", "tap", "fork",
+  "reduce", "map", "filter", "take", "flatten", "flatmap", "groupby",
+  "includes", "pluck",
+  "prop",
+  "cond", "defaultto",
 )
 
-"""
-Function
-"""
-def curry(f, wargs = (), wkwargs = {}):
+# Function
+def curry(f, wargs=[], wkwargs={}):
   @functools.wraps(f)
   def wrapper(*nargs, **nkwargs):
-    args = wargs + nargs
-    kwargs = {**wkwargs, **nkwargs}
+    args = wargs + list(nargs)
+    kwargs = {**nkwargs, **nkwargs}
     arity = f.__code__.co_argcount
     if len(args) + len(kwargs) >= arity:
       return f(*args, **kwargs)
-    return curry(f, args, kwargs)
+    return curry(f, args)
   return wrapper
 
+
+def _compose2(f, g):
+  return lambda *args: f(g(*args))
+
 def compose(*fns):
-  compose2 = lambda f, g: lambda *args, **kwargs: f(g(*args, **kwargs))
-  return functools.reduce(compose2, fns)
-
-def always(x):
   """
-  a -> (* -> a)
-  >>> always(42)()
-  42
+  >>> add3 = lambda x: x + 3
+  >>> mul2 = lambda x: x * 2
+  >>> square = lambda x: x * x
+  >>> pipe(add3, mul2, square)(1)
+  5
   """
-  return lambda: x
+  return functools.reduce(_compose2, fns)
 
+def _pipe2(f, g):
+  return lambda *args: g(f(*args))
+
+def pipe(*fns):
+  """
+  >>> add3 = lambda x: x + 3
+  >>> mul2 = lambda x: x * 2
+  >>> square = lambda x: x * x
+  >>> pipe(add3, mul2, square)(1)
+  64
+  """
+  return functools.reduce(_pipe2, fns)
+
+def T(*args):
+  return True
+
+def F(*args):
+  return False
+
+def memomize(f):
+  cache = {}
+  def _(*nargs, **nkwargs):
+    pass
+
+"""
+Function Combinators
+"""
 def identity(x):
   """
-  a -> a
   >>> identity(42)
   42
   """
   return x
 
+def always(x):
+  """
+  >>> always(42)()
+  42
+  """
+  return lambda *args, **kwargs: x
+
+@curry
+def alternation(f1, f2, x):
+  f1_result = f1(x)
+  return f1_result if f1_result else f2(x)
+
+@curry
+def tap(f, x):
+  """
+  >>> log = tap(print)
+  >>> log(10)
+  10
+  10
+  """
+  f(x)
+  return x
+
+@curry
+def fork(fns, joiner, x):
+  results = map(lambda f: f(x), fns)
+  return joiner(*results)
+
 """
 Iterator
 """
 @curry
+def reduce(initial, reducer, xs):
+  return functools.reduce(reducer, xs, initial)
+
+@curry
 def map(f, xs):
   """
-  >>> map(lambda x: x * 2, [1, 2, 3])
-  [2, 4, 6]
+  >>> addOne = lambda x: x + 1
+  >>> map(addOne, [1, 2, 3, 4])
+  [2, 3, 4, 5]
   """
   return list(builtins.map(f, xs))
 
-def flatten(xs):
+@curry
+def filter(pred, xs):
   """
-  [a] -> [b]
+  >>> iseven = lambda x: x % 2 == 0
+  >>> filter(iseven, [1, 2, 3, 4, 5, 6, 7])
+  [2, 4, 6]
+  """
+  return list(builtins.filter(pred, xs))
+
+@curry
+def take(number, xs):
+  """
+  >>> take3 = take(3)
+  >>> take3([1, 2, 3, 4, 5, 6, 7])
+  [1, 2, 3]
+  """
+  if number > len(xs):
+    return xs
+  return [xs[i] for i in range(0, number)]
+
+def flatten(xs):
+  """ flatten an array
   >>> flatten([[1, 2], [3, 4]])
   [1, 2, 3, 4]
   """
-  return functools.reduce(lambda acc, v: acc + v, xs)
+  return functools.reduce(lambda acc, v: [*acc, *v], xs, [])
 
 @curry
 def flatmap(f, xs):
-  """
-  >>> flatmap(lambda x: [x * 2], [1, 2, 3])
-  [2, 4, 6]
+  """ map and flatten an array (= flatten(map(f)))
+  >>> flatmap(lambda x: [2 * x], [1, 2, 3, 4])
+  [2, 4, 6, 8]
   """
   return compose(
     flatten,
     map(f),
   )(xs)
+
+@curry
+def pluck(key, xs):
+  """
+  >>> pluck_age = pluck("age")
+  >>> pluck_age([
+    {"name": "Alice", "age": 30},
+    {"name": "John", "age": 42},
+    {"name": "Eve", "age": 34},
+  ])
+  [30, 42, 34]
+  """
+  return map(prop(key), xs)
+
+@curry
+def includes(xs: list, x):
+  return x in xs
+
+@curry
+def groupby(key, xs):
+  """ Group a list by a key or a classifier function
+  from ftool import groupby
+  >>> groupby("gender", [{"gender": "M", "name": "John"}, {"gender": "F", "name": "Alice"}, {"gender": "M", "name": "N"}])
+  {'M': [{'gender': 'M', 'name': 'John'}, {'gender': 'M', 'name': 'N'}], 'F': [{'gender': 'F', 'name': 'Alice'}]}
+
+  >>> iseven = lambda x: x % 2 == 0
+  >>> groupby(iseven, [1, 2, 3, 4, 5, 6, 7, 8])
+  {False: [1, 3, 5, 7], True: [2, 4, 6, 8]}
+  """
+  classify = key if callable(key) else lambda x: x[key]
+  d = {}
+  for x in xs:
+    k = classify(x)
+    l = d.get(k, [])
+    l.append(x)
+    d[k] = l
+  return d
+
+"""
+Dict
+"""
+@curry
+def prop(name, o):
+  """
+  >>> get_name = prop("name")
+  >>> a = {"prop1": "value1", "name": "John"}
+  >>> get_name(a)
+  'John'
+  """
+  return o[name]
+
+"""
+Logic
+"""
+def cond(pairs):
+  """ Returns a function which encapsulates 'if/else', 'if/else' ... logic
+  >>> lt30 = lambda num: num < 30
+  >>> lt60 = lambda num: num < 60
+  >>> lt85 = lambda num: num < 85
+  >>>
+  >>> f = cond([
+    [lt30, "D"],
+    [lt60, "C"],
+    [lt85, "B"],
+    [T, "A"],
+  ])
+  >>> f(0)
+  'D'
+  >>> f(90)
+  'A'
+
+  """
+  def f(x):
+    for predicate, transformer in pairs:
+      if predicate(x):
+        return transformer(x) if callable(transformer) else transformer
+  return f
+
+@curry
+def defaultto(default, x):
+  return x if x else default
+
+"""
+Heigher level function
+"""
+@tap
+def log(x: str):
+  print(x)
--- test/test_core.py
+++ test/test_core.py
@@ -1,40 +1,125 @@
 from ftool import (
-  curry, compose,
-  always, identity,
-  map, flatten, flatmap,
+  compose, pipe, curry, T, F, identity, always, alternation, fork,
+  map, groupby, flatten, flatmap, pluck, includes,
+  prop, cond, defaultto,
 )
 
+"""
+Function
+"""
+def test_compose_simple():
+  add = lambda x, y: x + y
+  mul2 = lambda x: x * 2
+  assert compose(mul2, add)(1, 2) == 6
+
+def test_pipe_simple():
+  add = lambda x, y: x + y
+  mul2 = lambda x: x * 2
+  assert pipe(add, mul2)(1, 2) == 6
+
 def test_curry():
-  f = lambda x, y, z: x + y + z
-  assert curry(f)(1, 2, 39) == 42
-  assert curry(f)(1, 2)(39) == 42
-  assert curry(f)(1)(2)(39) == 42
+  add = curry(lambda x, y: x + y)
+  mul = curry(lambda x, y: x * y)
+  add3 = add(3)
+  mul6 = mul(6)
+  assert add3(39) == 42
+  assert mul6(7) == 42
 
-def test_compose():
-  assert compose(
-    lambda x: x + 2,
-    lambda x: x * 2,
-    lambda a, b: a + b
-  )(15, 5) == 42, "not 42"
+def test_t():
+  assert T()
 
-  assert compose(
-    lambda x: x + 2,
-    lambda x: x * 2,
-    lambda a=0, b=0: a + b
-  )(b=5, a=15) == 42
+def test_f():
+  assert not F()
 
-def test_always():
-  assert always(42)() == 42
+def test_alternation():
+  assert alternation(identity, F)(42) == 42
+  assert alternation(F, identity)(42) == 42
 
-def test_identity():
-  assert identity(42) == 42
+def test_fork():
+  f = lambda x: x + 1
+  g = lambda x: x * 2
+  h = lambda x: x * x
+  join = lambda x, y, z: y + z - x
+  assert fork([f, g, h], join)(6) == 41
 
+"""
+Iterator
+"""
 def test_map():
-  assert map(lambda x: x * 2, [1, 2, 3]) == [2, 4, 6]
+  assert map(lambda x: x)([1, 2, 3]) == [1, 2, 3]
 
 def test_flatten():
   assert flatten([[1, 2], [3, 4]]) == [1, 2, 3, 4]
 
 def test_flatmap():
-  assert flatmap(lambda x: [x * 2], [1, 2, 3]) == [2, 4, 6]
+  assert flatmap(lambda x: [2*x], [1, 2, 3, 4]) == [2, 4, 6, 8]
 
+def test_groupby():
+  assert groupby("gender", [
+    {'name': 'Alice', 'gender': 'F'},
+    {'name': 'Bob', 'gender': 'M'},
+    {'name': 'Charlie', 'gender': 'M'},
+  ]) == {
+    "F": [{'name': 'Alice', 'gender': 'F'}],
+    "M": [
+      {'name': 'Bob', 'gender': 'M'},
+      {'name': 'Charlie', 'gender': 'M'},
+    ]
+  }
+  def classify(score):
+    if score < 30:
+      return "D"
+    elif score < 60:
+      return "C"
+    elif score < 85:
+      return "B"
+    else:
+      return "A"
+  assert groupby(classify)([20, 60, 50, 95, 10, 45]) == {
+    "A": [95], "B": [60], "C": [50, 45], "D": [20, 10]
+  }
+
+def test_pluck():
+  pluck_age = pluck("age")
+  assert pluck_age([
+    {"name": "Alice", "age": 30},
+    {"name": "John", "age": 42},
+    {"name": "Eve", "age": 34},
+  ]) == [30, 42, 34]
+
+def test_includes():
+  is_in_name = includes(["John", "Alice", "Eve", "Bob"])
+  assert is_in_name("Eve")
+  assert not is_in_name("Daniel")
+
+"""
+Logic
+"""
+def test_cond():
+  f = cond([
+    [lambda x: x < 30, "D"],
+    [lambda x: x < 60, "C"],
+    [lambda x: x < 85, "B"],
+    [T, "A"],
+  ])
+  assert f(0) == "D"
+  assert f(30) == "C"
+  assert f(40) == "C"
+  assert f(80) == "B"
+  assert f(99) == "A"
+
+  g = cond([
+    [lambda x: x < 30, lambda x: str(x) + ": D"],
+    [lambda x: x < 60, lambda x: str(x) + ": C"],
+    [lambda x: x < 85, lambda x: str(x) + ": B"],
+    [T, lambda x: str(x) + ": A"],
+  ])
+  assert g(0) == "0: D"
+  assert g(30) == "30: C"
+  assert g(40) == "40: C"
+  assert g(80) == "80: B"
+  assert g(99) == "99: A"
+
+def test_defaultto():
+  assert defaultto(100)(42) == 42
+  assert defaultto(42)(None) == 42
