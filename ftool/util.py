50572602fd9cbfdd910c72831d3fe6b463eb5b9b
r-tamura <rtamura30@gmail.com>
2018-12-24 13:37:23 +0900

    Add log

--- ftool/util.py
+++ ftool/util.py
@@ -1,8 +1,13 @@
+from . import tap
 
-__all__ = ("inspect",)
+__all__ = ("inspect", "log")
 
 def inspect(x):
   if hasattr(x, "inspect"):
     return x.inspect()
   else:
     return x
+
+@tap
+def log(x):
+  print(x)
